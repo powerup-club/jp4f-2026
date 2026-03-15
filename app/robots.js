dffcd1e78956819ac2199f8c96cbf384c0572702
Power UP <powerup.ensa@usmba.ac.ma>
2026-03-15 22:50:58 +0000

    add static sitemap

--- app/robots.js
+++ app/robots.js
@@ -1,43 +1,10 @@
-const BASE_URL = "https://jp4f.vercel.app";
-
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "Googlebot",
-        allow: "/"
-      },
-      {
-        userAgent: "GPTBot",
-        allow: "/"
-      },
-      
-      {
-        userAgent: "ClaudeBot",
-        allow: "/"
-      },
-      {
-        userAgent: "Bingbot",
-        allow: "/"
-      },
-      {
-        userAgent: "DuckDuckBot",
-        allow: "/"
-      },
-      {
-        userAgent: "YandexBot",
-        allow: "/"
-      },
-      {
-        userAgent: "Baiduspider",
-        allow: "/"
-      },
-      {
-        userAgent: "PerplexityBot",
-        allow: "/"
-      }
-    ],
-    sitemap: 'https://innov-indus.vercel.app/sitemap.xml',
-  };  
+    rules: {
+      userAgent: "*",
+      allow: "/"
+    },
+    sitemap: "https://innov-indus.vercel.app/sitemap.xml"
+  };
 }
 
